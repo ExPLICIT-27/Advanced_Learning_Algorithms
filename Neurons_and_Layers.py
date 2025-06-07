e53207e278428014b5a08454e3747141ca3d10ae
Explicit_27 <nandumahesh2004@gmail.com>
2025-06-07 10:00:52 +0530

    Neural network example

--- Neurons_and_Layers.py
+++ Neurons_and_Layers.py
@@ -49,3 +49,51 @@ print(a1)
 alin = np.dot(set_w, X_train[0].reshape(1, 1)) + set_b
 print(alin)
 
+#now comparing the tensorflow and linear model predictions for the entire dataset
+prediction_tf = linear_layer(X_train)
+prediction_np = np.dot(X_train, set_w) + set_b
+
+plt_linear(X_train, Y_train, prediction_tf, prediction_np)
+
+#neural network with sigmoid activation
+X_train = np.array([0., 1, 2, 3, 4, 5], dtype=np.float32).reshape(-1,1)  # 2-D Matrix
+Y_train = np.array([0,  0, 0, 1, 1, 1], dtype=np.float32).reshape(-1,1)  # 2-D Matrix
+
+pos = Y_train == 1
+neg = X_train == 0
+
+fig, ax = plt.subplots(1, 1, figsize = (4, 3))
+ax.scatter(X_train[pos], Y_train[pos], marker = 'x', s=80, c = 'red', label = 'y1')
+ax.scatter(X_train[neg], Y_train[neg], marker = 'o', s=100, label = 'y=0', facecolors = 'none', edgecolors=dlc["dlblue"],lw=3)
+
+ax.set_ylim(-0.08,1.1)
+ax.set_ylabel('y', fontsize=12)
+ax.set_xlabel('x', fontsize=12)
+ax.set_title('one variable plot')
+ax.legend(fontsize=12)
+plt.show()
+
+#creating a single layered logistic neuron
+model = Sequential(
+    [
+        tf.keras.layers.Dense(1, input_dim = 1, activation = 'sigmoid', name = 'L1')
+    ]
+)
+model.summary()
+
+logistic_layer = model.get_layer('L1')
+w, b = logistic_layer.get_weights()
+print(w, b)
+print(w.shape, b.shape)
+
+#setting weights to known values
+set_w = np.array([[2]])
+set_b = np.array([-4.5])
+
+logistic_layer.set_weights([set_w, set_b])
+print(logistic_layer.get_weights())
+
+a1 = model.predict(X_train[0].reshape(1, 1))
+print(a1)
+alog = sigmoidnp(np.dot(X_train[0].reshape(1, 1), set_w) + set_b)
+plt_logistic(X_train, Y_train, model, set_w, set_b, pos, neg)
